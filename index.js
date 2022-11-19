43d030dec39fe0778993d5e565b737c118fa3aea
Bob Oyier <boboyier92@gmail.com>
2022-11-19 19:58:59 +0300

    "Completed Moringa School's looping lab."

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+// const messages = []
+// function writeCards(name, eventName) {
+// 	for (let i = 0; i < name.length; i++) {
+// 		messages.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+// 	}
+// 	return messages;
+// }
+
+// writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+// console.log(messages);
+
+const messages = [];
+function writeCards(name, eventName) {
+	for (let i = 0; i < name.length; i++) {
+		messages[i] = `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`;
+	}
+	return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(messages);
+
+function countDown(number) {
+	while (number >= 0) {
+		console.log(number--)
+	}
+}
+
+countDown(10)
